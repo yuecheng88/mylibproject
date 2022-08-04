8c0cad7c6d8d7b141338cfa79e1db045b38f9dc7
xiaoweideng <dengxiaowei@laiye.com>
2022-08-04 18:01:23 +0800

    发顺丰

---
+++ src/main/java/com/xiao/util/StrUtils.java
@@ -0,0 +1,13 @@
+package com.xiao.util;
+
+import java.io.UnsupportedEncodingException;
+
+public class StrUtils {
+
+
+    public static void main(String[] args) {
+        String s = "123abc计算" ;
+        System.out.println(s.getBytes().length);
+        System.out.println(s.length());
+    }
+}
